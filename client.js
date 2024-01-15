31fb7554adb16d9572f4ac49e61b5ead79f23b36
tik9 <tik9@users.noreply.github.com>
2024-01-15 14:14:53 +0000

    deploy: cbd49e1c328c45c13c15349fdb1414ec3a9a2798

--- client.js
+++ client.js
@@ -1,27 +1,24 @@
 
 document.title = tiko
 
-// var net_host = 'http://localhost'
-var net_host = 'https://tifun.netlify.app'
+var net_host = 'http://localhost'
+// var net_host = 'https://tifun.netlify.app'
 var net_fun = '/.netlify/functions/'
 
 data()
 async function data() {
-    var data = 'data'
-    // var res = await (await fetch(net_host + net_fun + 'mongo?op=find&coll=' + data)).json();
-    // var res = [{ text: 'Alice', cat: 21 },];
-    var utils = 'utils'
-    var res = await (await (fetch(net_host + net_fun + utils))).json()
+    var res = await (await (fetch(net_host + net_fun + 'utils'))).json()
 
     // console.log(res)
 
     var main_div = document.createElement('div')
-    main_div.id = data
+    main_div.id = 'data'
     container.append(main_div)
     var list = document.createElement('ul')
     main_div.append(list)
 
     res = groupBy(res, 'cat');
+    // console.log(2, res)
 
     for (var elem in res) {
         var head = document.createElement('h5')
